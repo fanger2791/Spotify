7aca180ca2a03b96230da40052253e7c0bc852f0
fanger2791 <m.fang@mail.utoronto.ca>
2024-02-29 10:38:52 -0500

    updated with fifth harmony data

--- scripts/download_data_spotify.R
+++ scripts/download_data_spotify.R
@@ -9,3 +9,13 @@ beyonce <- get_artist_audio_features("beyonce")
 
 saveRDS(beyonce, "data/raw_data/beyonce.rds")
 
+#### Workspace setup ####
+library(tidyverse)
+library(spotifyr)
+
+
+#### Download data ####
+fifth_harmony <- get_artist_audio_features("fifth harmony")
+
+#### Save data ####
+saveRDS(fifth_harmony, "data/raw_data/fifth_harmony.rds")
