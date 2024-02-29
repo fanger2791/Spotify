28f020c4ca6525d8ee74c000b97e71c86d26efd0
fanger2791 <m.fang@mail.utoronto.ca>
2024-02-29 09:36:30 -0500

    ADD nasa content

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from NASA API
 # Author: Rohan Alexander [...UPDATE THIS...]
 # Date: 11 February 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
@@ -9,18 +9,18 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(httr)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+NASA_APOD_20190719 <-
+  GET("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-19")
 
+# APOD July 19, 2019
+content(NASA_APOD_20190719)$date
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+content(NASA_APOD_20190719)$explanation
+
 
          
